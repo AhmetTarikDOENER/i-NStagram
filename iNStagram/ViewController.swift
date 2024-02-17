f5f35b213644d95edc9ba7b09e5ad5442f140470
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2024-02-17 18:49:33 +0300

    Started to create i-NStagram project.

---
+++ iNStagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iNStagram
+//
+//  Created by Ahmet Tarik DÖNER on 17.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
