e8193dba087b839e01885b5dfb84136f619a69f3
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2024-02-20 23:44:25 +0300

    Added skeleton collection view for home view controller.

--- iNStagram/Controllers/Core/HomeViewController.swift
+++ iNStagram/Controllers/Core/HomeViewController.swift
@@ -8,11 +8,118 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    private var collectionView: UICollectionView?
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Instagram"
         view.backgroundColor = .systemBackground
+        configureCollectionView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView?.frame = view.bounds
+    }
+    
+    //MARK: - Private
+    private func configureCollectionView() {
+        let sectionHeight: CGFloat = 240 + view.width
+        let collectionView = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: UICollectionViewCompositionalLayout(sectionProvider: {
+                index,
+                _ -> NSCollectionLayoutSection? in
+                // Items
+                let posterItem = NSCollectionLayoutItem(
+                    layoutSize: NSCollectionLayoutSize(
+                        widthDimension: .fractionalWidth(1.0),
+                        heightDimension: .absolute(60)
+                    )
+                )
+                let postItem = NSCollectionLayoutItem(
+                    layoutSize: NSCollectionLayoutSize(
+                        widthDimension: .fractionalWidth(1.0),
+                        heightDimension: .fractionalWidth(1.0)
+                    )
+                )
+                let actionsItem = NSCollectionLayoutItem(
+                    layoutSize: NSCollectionLayoutSize(
+                        widthDimension: .fractionalWidth(1.0),
+                        heightDimension: .absolute(40)
+                    )
+                )
+                let likeCountItem = NSCollectionLayoutItem(
+                    layoutSize: NSCollectionLayoutSize(
+                        widthDimension: .fractionalWidth(1.0),
+                        heightDimension: .absolute(40)
+                    )
+                )
+                let captionItem = NSCollectionLayoutItem(
+                    layoutSize: NSCollectionLayoutSize(
+                        widthDimension: .fractionalWidth(1.0),
+                        heightDimension: .absolute(60)
+                    )
+                )
+                let timestampsItem = NSCollectionLayoutItem(
+                    layoutSize: NSCollectionLayoutSize(
+                        widthDimension: .fractionalWidth(1.0),
+                        heightDimension: .absolute(40)
+                    )
+                )
+                // Group
+                let group = NSCollectionLayoutGroup.vertical(
+                    layoutSize: NSCollectionLayoutSize(
+                        widthDimension: .fractionalWidth(1.0),
+                        heightDimension: .absolute(sectionHeight)
+                    ),
+                    subitems: [
+                        posterItem, postItem, actionsItem, likeCountItem, captionItem, timestampsItem
+                    ]
+                )
+                // Sections
+                let section = NSCollectionLayoutSection(group: group)
+                section.contentInsets = NSDirectionalEdgeInsets(
+                    top: 3,
+                    leading: 0,
+                    bottom: 15,
+                    trailing: 0
+                )
+                return section
+            })
+        )
+        view.addSubviews(collectionView)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        self.collectionView = collectionView
     }
 }
-
+    
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        let colors = [
+            UIColor.red,
+            UIColor.green,
+            UIColor.blue,
+            UIColor.yellow,
+            UIColor.systemPink,
+            UIColor.orange
+        ]
+        cell.contentView.backgroundColor = colors[indexPath.row]
+        return cell
+    }
+}
